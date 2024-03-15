7d7b5dc16a510f6b3fe90e88860f88b1b6dec979
NineNero <salmaurbano01@gmail.com>
2024-03-15 10:18:20 +0100

    Canva

---
+++ Assets/Scripts/ScriptableItem.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Items/Menu Item")]
+
+public class ScriptableItem : ScriptableObject
+{
+    public enum ItemType
+    {
+        Weapon,
+        Armor,
+        Shield
+    }
+    public ItemType itemType;
+
+    public string itemName;
+    public string itemDescription;
+    public int itemPrice;
+    public Sprite itemSprite;
+}
